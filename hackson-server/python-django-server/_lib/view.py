ea3565dfb242b857119753e74550989dcd6bfb24
guowenchaode <guowenchaode@126.com>
2023-09-21 16:31:21 +0800

    init-modal

--- hackson-server/python-django-server/_lib/view.py
+++ hackson-server/python-django-server/_lib/view.py
@@ -1,5 +1,9 @@
 from django.http import JsonResponse
 
 def resp(data):
-    resp = {"data":data,"code":"111"}
+    resp = {"data":data,"code":0}
+    return JsonResponse(resp, safe=False)
+
+def resp_err(code, message):
+    resp = {"data":{},"code":code, "message":message}
     return JsonResponse(resp, safe=False)
