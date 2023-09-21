b51ff66ac49f2370d060cd113dfc4dd29356e5af
guowenchaode <guowenchaode@126.com>
2023-09-21 14:20:18 +0800

    init-lib-name

--- hackson-server/python-django-server/_databricks/data_bricks_client.py
+++
@@ -1,8 +0,0 @@
-from databricks_api import DatabricksAPI
-
-# Provide a host and token
-db = DatabricksAPI(
-    host="example.cloud.databricks.com",
-    token="dpapi123..."
-)
-
---
+++ hackson-server/python-django-server/_databricks/workspace.py
@@ -0,0 +1,10 @@
+from databricks_api import DatabricksAPI
+
+# Provide a host and token
+
+def connect_workspace():
+    db = DatabricksAPI(
+        host="adb-8728304164668675.15.azuredatabricks.net",
+        token="dapi12e93279816c5caf6fdc86b7089dc6ca")
+
+    return db
