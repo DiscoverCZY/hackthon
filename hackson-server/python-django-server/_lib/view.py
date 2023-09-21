18723578be99ba388650d5f63aa253ea9d30e99a
guowenchaode <guowenchaode@126.com>
2023-09-21 16:36:13 +0800

    init-modal

--- hackson-server/python-django-server/_lib/view.py
+++ hackson-server/python-django-server/_lib/view.py
@@ -4,6 +4,6 @@ def resp(data):
     resp = {"data":data,"code":0}
     return JsonResponse(resp, safe=False)
 
-def resp_err(code, message):
-    resp = {"data":{},"code":code, "message":message}
+def resp_err(message,code=100):
+    resp = {"data":{},"code":code, "message":f'{message}'}
     return JsonResponse(resp, safe=False)
--- hackson-server/python-django-server/apis/rest_api/workflow_run_views.py
+++ hackson-server/python-django-server/apis/rest_api/workflow_run_views.py
@@ -1,14 +1,20 @@
 from django.http import HttpResponse, JsonResponse
 from rest_framework.views import APIView
 from _databricks.workflow_run import start_workspace, load_list
-from _lib.view import resp
+from _lib.view import resp,resp_err
 
 mock_data={"id":"xxx"}
+ERROR_FAIL_LOAD_LIST='Internal Error: fail to load run list'
+ERROR_FAIL_LOAD_LIST_CODE=100
 
 class WorkflowRunView(APIView):
     def get(self, request):
-        data = load_list(request)
-        return resp(data)
+        try:
+            data = load_list(request)
+            return resp(data)
+        except Exception as e:
+            return resp_err(ERROR_FAIL_LOAD_LIST,ERROR_FAIL_LOAD_LIST_CODE)
+        
 
     def post(self, request):
         data = start_workspace(request.data)
